a4de15830c1824464810d7a1b58e1fe474b5eb1d
Christopher Neugebauer <chrisjrn@gmail.com>
2016-03-04 12:22:01 -0800

    Adds checkout view, which generates an invoice, and then redirects to the invoice itself.

--- registrasion/urls.py
+++ registrasion/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     "registrasion.views",
     url(r"^category/([0-9]+)$", "product_category", name="product_category"),
-    #    url(r"^category$", "product_category", name="product_category"),
+    url(r"^checkout$", "checkout", name="checkout"),
+    url(r"^invoice/([0-9]+)$", "invoice", name="invoice"),
 )
--- registrasion/views.py
+++ registrasion/views.py
@@ -1,11 +1,13 @@
 from registrasion import forms
 from registrasion import models as rego
 from registrasion.controllers.cart import CartController
+from registrasion.controllers.invoice import InvoiceController
 
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.forms import formset_factory
+from django.shortcuts import redirect
 from django.shortcuts import render
 from functools import partial, wraps
 
@@ -55,3 +57,28 @@ def product_category(request, category_id):
     }
 
     return render(request, "product_category.html", data)
+
+@login_required
+def checkout(request):
+    ''' Runs checkout for the current cart of items, ideally generating an
+    invoice. '''
+
+    current_cart = CartController.for_user(request.user)
+    current_invoice = InvoiceController.for_cart(current_cart.cart)
+
+    return redirect("invoice", current_invoice.invoice.id)
+
+
+@login_required
+def invoice(request, invoice_id):
+    ''' Displays an invoice for a given invoice id. '''
+
+    invoice_id = int(invoice_id)
+    inv = rego.Invoice.objects.get(pk=invoice_id)
+    current_invoice = InvoiceController(inv)
+
+    data = {
+        "invoice": current_invoice.invoice,
+    }
+
+    return render(request, "invoice.html", data)

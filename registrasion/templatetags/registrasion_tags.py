c192fef491c4b58dfddea21c7fbffbd8b2a9e5bc
Christopher Neugebauer <chrisjrn@gmail.com>
2016-03-25 14:16:30 +1100

    Adds basic template tag for available categories. Currently does not check enabling conditions.

---
+++ registrasion/templatetags/registrasion_tags.py
@@ -0,0 +1,10 @@
+from registrasion import models as rego
+
+from django import template
+
+register = template.Library()
+
+@register.assignment_tag(takes_context=True)
+def available_categories(context):
+    ''' Returns all of the available product categories '''
+    return rego.Category.objects.all()
